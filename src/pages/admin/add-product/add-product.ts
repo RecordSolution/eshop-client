6b2738c2c645aee6fe264e6dacd731e9531296e2
Eddy <adnanramzan0001@gmail.com>
2019-03-25 23:09:30 +0500

    latest code

--- src/pages/admin/add-product/add-product.ts
+++ src/pages/admin/add-product/add-product.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the AddProductPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+//  import { addProductModel } from './addProductModel/addProductModel';
+
+
 
 @IonicPage()
 @Component({
@@ -15,15 +12,77 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AddProductPage {
 
+  // public avaliableSizes : [{'SM'},{'MD','LG','XL']
+  // avaliableSizes :[ {'SM'},{'MD'},{'LG'},{'XL'},  ]
+  avaliableSizes: Array<any>=['SM','MD','LG','XL'];
+  Size : string;
+  dressCategory : string;
+  showSizes : Array<any>
+
+  // addProducts : addProductModel;
+
+  public  addProductModel:{name : string, id : string,shortDescription:string,colors : Array<string>,avaliableQuantity: string,images : Array<any>,dressCategory : string,fashionCatehory : string, gendar : string,sizes : Array<string>,price : string}={
+    id : '',
+    name :'',
+    shortDescription : '',
+    colors : [],
+    avaliableQuantity : '',
+    images : [],
+    dressCategory : '',
+    fashionCatehory : '',
+    gendar : '',
+    sizes :[],
+    price : ''
+
+
+
+
+
+  }
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    // console.log(this.avaliableSizes)
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad AddProductPage');
+    console.log();
   }
 
-  filepicker(event){
-    console.log(event)
+  filepicker(file){
+    this.fileToBase64(file[0]).then(base64 => {console.log(base64)})
+   
   }
 
+  addProduct(){
+    console.log('Here')
+  }
+
+  SelectedSize(){
+    console.log('here')
+   this.addProductModel.sizes.push(this.Size)
+// this.showSizes.push(this.Size)
+   console.log(this.addProductModel.sizes)
+
+
+  }
+  SelectedDressCategory(){
+
+  }
+
+  async fileToBase64(file: File) {
+
+    return new Promise(
+
+        (resolve, reject) => {
+            let reader = new FileReader();
+            reader.onload = (e: any) => {
+                //   if (error) reject(error);
+                //   let content = JSON.parse(data);
+                //           let fact = content.value;
+                resolve(e.target.result);
+                // })
+            }
+            reader.readAsDataURL(file);
+        });
+}
 }
---
+++ src/pages/admin/add-product/addProductModel/addProductModel.ts
@@ -0,0 +1,3 @@
+export class addProductModel {
+    name : string;
+}
